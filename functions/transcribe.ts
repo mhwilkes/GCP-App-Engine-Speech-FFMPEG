b1c8f709f812386c88d12d6518363f48b423065c
Mike <mhwilkes@gmail.com>
2020-12-16 20:33:13 -0500

    transcribe Documentation

--- functions/transcribe.ts
+++ functions/transcribe.ts
@@ -1,30 +1,43 @@
 import * as Speech from '@google-cloud/speech';
 import { google } from '@google-cloud/speech/build/protos/protos';
+import * as protos from '@google-cloud/speech/build/protos/protos';
 import IRecognizeRequest = google.cloud.speech.v1.IRecognizeRequest;
 import IRecognitionConfig = google.cloud.speech.v1.IRecognitionConfig;
 import IRecognitionAudio = google.cloud.speech.v1.IRecognitionAudio;
 
-const ENCODING = 'LINEAR16'; // was 'LINEAR16'
+// Settings used for Transcription Configuration
+const ENCODING = 'LINEAR16';
 const SAMPLE_RATE_HERTZ = 16000;
 const LANGUAGE = 'en-US';
 
+// Recognition Config for Speech API
 const config: IRecognitionConfig = {
   encoding: ENCODING,
   sampleRateHertz: SAMPLE_RATE_HERTZ,
   languageCode: LANGUAGE,
   audioChannelCount: 1,
 };
-
+/**
+ * Takes a base64 encoded string containing the transcode voice data from the client,
+ * and send to the Speech API to return a Promise containing the transcription.
+ *
+ * @param base64Data Base64 encoded string containing transcode voice data to spec
+ * requested by Speech API
+ */
 const transcribeAudioStream = (base64Data : string) => {
+  // Set RecognitionAudio Object content to base64Data
   const audio: IRecognitionAudio = {
     content: base64Data,
   };
+  // Create a RecognizeRequest with config and audio
   const request: IRecognizeRequest = {
     config,
     audio,
   };
+  // Setup Speech Client
   const speech = new Speech.SpeechClient();
 
+  // Return Promise from Speech Client
   return speech.recognize(request).then((response) => response).catch((error) => {
     console.log('SPEECH error:', error);
   });
--- index.ts
+++ index.ts
@@ -1,13 +1,20 @@
-import * as express from 'express';
 import * as bodyParser from 'body-parser';
-import * as multer from 'multer';
 import * as cors from 'cors';
+import * as express from 'express';
 import * as fs from 'fs';
+import * as multer from 'multer';
 import { Readable } from 'stream';
 
+// eslint-disable-next-line import/extensions,import/no-unresolved
 import transcodeAudioStream from './functions/transcode';
+// eslint-disable-next-line import/extensions,import/no-unresolved
 import transcribeAudioStream from './functions/transcribe';
 
+/**
+ * Creates a Readable Stream from the Buffer Provided by MemoryStorage through multer
+ *
+ * @param buffer Buffer containing entire File from MemoryStorage uploaded through multer.
+ */
 const bufferToReadableStream = (buffer: Buffer) => {
   const stream = new Readable();
   stream.push(buffer);
@@ -15,32 +22,46 @@ const bufferToReadableStream = (buffer: Buffer) => {
   return stream;
 };
 
+// Port can be set through .env variable or to default 8080
 const PORT = Number(process.env.PORT) || 8080;
 
 const app = express();
 
+// Using Dependencies bodyparser and cors
 app.use(bodyParser.json());
 app.use(cors());
 
+// Default endpoint gives empty page
 app.get('/', (_req, res) => {
   res.send('🎉 Hello TypeScript! 🎉');
 });
 
+// Solution Endpoint for Transcription. Current upload limit is 5 MB but can be modified below
+// Multer is used to handle file upload parsing
 app.post('/solution', multer({
+  // Storage of file is entirely within memory in form of Buffer
   storage: multer.memoryStorage(),
   limits: {
-    fileSize: 5 * 1024 * 1024, // no larger than 5mb, you can change as needed.
+    fileSize: 5 * 1024 * 1024, // no larger than 5 MB, you can change as needed.
   },
 }).single('file'), (req, res) => {
+  // Convert Buffer to Stream
   const inStream = bufferToReadableStream(req.file.buffer);
 
+  // Transcoding Promise Function
   transcodeAudioStream(inStream).then(() => {
+    // Function writes results currently to test.wav so we read result into base64 string
     const data = fs.readFileSync('./test.wav').toString('base64');
+    // Pass base64 encoded string of transcode voice data for transcription
     transcribeAudioStream(data).then((response) => {
+      // Response contains transcription from Google Cloud Speech API
       const transcript = response[0].results.map(
         (result) => result.alternatives[0].transcript,
       ).join('\n');
+      // Remove temporary file
       fs.unlinkSync('./test.wav');
+
+      // Send client transcription response
       res.send({ transcript });
     });
   });
